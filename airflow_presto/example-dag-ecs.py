aeada913736d7add32f7ef73df96467d564259c0
Soren Archibald <soren@qbizinc.com>
2020-10-09 08:13:51 -0700

    Renamed example DAG files with extensions the reflect the AWS service they use.

---
+++ airflow_presto/example-dag-ecs.py
@@ -0,0 +1,55 @@
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from operators.presto_ecs_operator import ECSOperator
+
+from datetime import datetime, timedelta
+from airflow.hooks.presto_hook import PrestoHook
+import json, uuid
+
+with open('settings.json', 'r') as f:
+    settings = json.load(f)
+
+
+# Default settings applied to all tasks
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+# Using a DAG context manager, you don't have to specify the dag property of each task
+with DAG('example_dag',
+         start_date=datetime(2020, 10, 6),
+         max_active_runs=1,
+         schedule_interval=timedelta(days=1),  # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
+         default_args=default_args,
+         catchup=True # enable if you don't want historical dag runs to run
+         ) as dag:
+
+    t0 = DummyOperator(
+        task_id='start'
+    )
+    t1 = ECSOperator(
+        region_name=settings["region_name"],
+        aws_access_key_id=settings["aws_access_key_id"],
+        aws_secret_access_key=settings["aws_secret_access_key"],
+        task_id=settings["task_id"],
+        cluster=settings["cluster"],
+        count=settings["count"],
+        group=settings["group"],
+        launchType=settings["launchType"],
+        networkConfiguration=settings["networkConfiguration"],
+        overrides=settings["overrides"],
+        referenceId=settings["referenceId"],
+        # This ID is how we know what to spin down when we are finished
+        startedBy='airflow-' + str(uuid.uuid4()) ,
+        taskDefinition=settings["taskDefinition"],
+        query='select * from default.ny_pub LIMIT 10;'
+    )
+
+    t0 >> t1
--- airflow_presto/example-dag-kubernetes.py
+++ airflow_presto/example-dag-kubernetes.py
@@ -2,14 +2,22 @@ from airflow import DAG
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
-from operators.presto_ecs_operator import ECSOperator
-
 from datetime import datetime, timedelta
 from airflow.hooks.presto_hook import PrestoHook
-import json, uuid
 
-with open('settings.json', 'r') as f:
-    settings = json.load(f)
+
+
+def my_custom_function(ts,**kwargs):
+    """
+    This can be any python code you want and is called from the python operator. The code is not executed until
+    the task is run by the airflow scheduler.
+    """
+    print(f"I am task number {kwargs['task_number']}. This DAG Run execution date is {ts} and the current time is {datetime.now()}")
+    print(kwargs)
+    ph = PrestoHook(presto_conn_id='test')
+    hql = 'select * from system.runtime.nodes;'
+    records = ph.get_records(hql)
+    print(records)
 
 
 # Default settings applied to all tasks
@@ -24,32 +32,47 @@ default_args = {
 
 # Using a DAG context manager, you don't have to specify the dag property of each task
 with DAG('example_dag',
-         start_date=datetime(2020, 10, 6),
-         max_active_runs=1,
-         schedule_interval=timedelta(days=1),  # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
+         start_date=datetime(2019, 1, 1),
+         max_active_runs=3,
+         schedule_interval=timedelta(minutes=30),  # https://airflow.apache.org/docs/stable/scheduler.html#dag-runs
          default_args=default_args,
-         catchup=True # enable if you don't want historical dag runs to run
+         # catchup=False # enable if you don't want historical dag runs to run
          ) as dag:
 
     t0 = DummyOperator(
         task_id='start'
     )
-    t1 = ECSOperator(
-        region_name=settings["region_name"],
-        aws_access_key_id=settings["aws_access_key_id"],
-        aws_secret_access_key=settings["aws_secret_access_key"],
-        task_id=settings["task_id"],
-        cluster=settings["cluster"],
-        count=settings["count"],
-        group=settings["group"],
-        launchType=settings["launchType"],
-        networkConfiguration=settings["networkConfiguration"],
-        overrides=settings["overrides"],
-        referenceId=settings["referenceId"],
-        # This ID is how we know what to spin down when we are finished
-        startedBy='airflow-' + str(uuid.uuid4()) ,
-        taskDefinition=settings["taskDefinition"],
-        query='select * from default.ny_pub LIMIT 10;'
+    t1 = PythonOperator(
+        task_id = 'test_presto',
+        python_callable=my_custom_function,
+        op_kwargs={'task_number': 2},
+        provide_context=True
     )
 
+    '''
+    t1 = DummyOperator(
+        task_id='group_bash_tasks'
+    )
+    t2 = BashOperator(
+        task_id='bash_print_date1',
+        bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s && date')
+    t3 = BashOperator(
+        task_id='bash_print_date2',
+        bash_command='sleep $[ ( $RANDOM % 30 )  + 1 ]s && date')
+
+    # generate tasks with a loop. task_id must be unique
+    for task in range(5):
+        tn = PythonOperator(
+            task_id=f'python_print_date_{task}',
+            python_callable=my_custom_function,  # make sure you don't include the () of the function
+            op_kwargs={'task_number': task},
+            provide_context=True
+        )
+
+
+        t0 >> tn # indented inside for loop so each task is added downstream of t0
+
+    t0 >> t1
+    t1 >> [t2, t3] # lists can be used to specify mutliple tasks
+    '''
     t0 >> t1
